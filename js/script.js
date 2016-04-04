49d5d58d33ceaab3534ad626f8fdd6466c9544f5
Jesse Rohland <jesse.rohland@gmail.com>
2016-04-03 23:51:25 -0400

    v2

---
+++ js/script.js
@@ -0,0 +1,28 @@
+$(window).scroll(function() {
+  if ($(document).scrollTop() > 50) {
+    $('#header').addClass('shrink');
+    $('#header-spacer').addClass('shrink');
+  } else {
+    $('#header').removeClass('shrink');
+    $('#header-spacer').removeClass('shrink');
+  }
+});
+
+$("#header a[href^='#']").on('click', function(e) {
+   // prevent default anchor click behavior
+   e.preventDefault();
+
+   // store hash
+   var hash = this.hash;
+
+   // animate
+   $('html, body').animate({
+       scrollTop: $(this.hash).offset().top
+     }, 500, function(){
+
+       // when done, add hash to url
+       // (default click behaviour)
+       window.location.hash = hash;
+     });
+
+});
